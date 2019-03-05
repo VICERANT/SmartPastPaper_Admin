81461205e5f1cb6a57c9ae18131d11a39e3b215f
Janitha <janitharasanga133@gmail.com>
2019-03-05 20:45:28 +0530

    side bar

--- src/app/main-layout/navigation/navigation.component.ts
+++ src/app/main-layout/navigation/navigation.component.ts
@@ -19,17 +19,33 @@ export class NavigationComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+
+    for (i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+        } else {
+          dropdownContent.style.display = "block";
+        }
+      });
+    }
   }
 
   setClicked(val: boolean): void {
     this.clicked = val;
   }
 
-  logOut(){
+  logOut() {
     localStorage.removeItem('token');
     this.router.navigate(['/login']);
   }
 
-  
+
 
 }
